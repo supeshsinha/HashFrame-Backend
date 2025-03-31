b1a249287bb40e17972a7fcddb459c5d246e450f
Supesh <sinha.supesh@gmail.com>
2025-03-31 21:11:52 +0530

    fix

--- server.js
+++ server.js
@@ -49,4 +49,4 @@ app.get("/check/:hash", async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(process.env.PORT || 3000, () => console.log("Server running on port 3000"));
